793059d7c951dfc96e82f7d80d00a48c2963bcd0
Jose Zacarias <jose.zacarias@kcom.com>
2018-10-12 14:41:28 +0100

    Improvment on Exercise2. Variable name and While loop

--- src/practica2/Exercise2.java
+++ src/practica2/Exercise2.java
@@ -7,43 +7,46 @@ public class Exercise2 {
 
     public static void main(String[] args) {
     
-    int j;
-    int id;
-    String Desc;
-    Scanner ti = new Scanner (System.in);
-    Scanner des = new Scanner (System.in);
+    int optionCase;
+    int ticketId;
+    String descriptionTicket;
+    Scanner scannInput = new Scanner (System.in);
+    //Scanner des = new Scanner (System.in);
     System.out.println ("Submit a ticket:");
     System.out.println ("Press 1 to submit a Service Request or 2 to submit an Incident");
-    j = ti.nextInt();
-    switch (j){
+    optionCase = scannInput.nextInt();
+    while (optionCase == 1 || optionCase == 2) {
+           
+    switch (optionCase){
         case 1:
             exercise2SR service = new exercise2SR();
             Random i = new Random ();
-            id = i.nextInt(1 + 20)+1;
+            ticketId = i.nextInt(1 + 20)+1;
             System.out.println("Introduce the description: ");
-            Desc = des.nextLine ();
-            service.Assign(id, Desc);
+            descriptionTicket = scannInput.nextLine ();
+            service.Assign(ticketId, descriptionTicket);
             System.out.println("Your ticket information below: ");
             System.out.println ("ID: "+service.getIDR());
             System.out.println ("Description: "+service.getDescR());
             System.out.println ("Assigned Group: "+service.getGroup());
             break;
         case 2:
-            exercise2I inci = new exercise2I();
+            exercise2I incident = new exercise2I();
             Random k = new Random();
-            id = k.nextInt(1 + 20)+1;
+            ticketId = k.nextInt(1 + 20)+1;
             System.out.println("Introduce la descripcion: ");
-            Desc = des.nextLine ();
-            inci.Assign(id, Desc);
+            descriptionTicket = scannInput.nextLine ();
+            incident.Assign(ticketId, descriptionTicket);
             System.out.println("Your ticket information below: ");
-            System.out.println ("ID: "+inci.getIDR());
-            System.out.println ("Description: "+inci.getDescR());
-            System.out.println ("Assigned Group: "+inci.getGroup());
-            System.out.println ("Priority: "+inci.getPri());
+            System.out.println ("ID: "+incident.getIDR());
+            System.out.println ("Description: "+incident.getDescR());
+            System.out.println ("Assigned Group: "+incident.getGroup());
+            System.out.println ("Priority: "+incident.getPri());
             break;
             
         }
-                
+     }
+    System.out.println ("You selected a wrong option");
     }
     
 }
