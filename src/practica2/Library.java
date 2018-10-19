a7c83d477eb517bd54da90e59ffb4c588ce59c98
Jose Zacarias <jose.zacarias@kcom.com>
2018-10-19 10:20:15 +0100

    Exception code added to Library project

--- src/practica2/Library.java
+++ src/practica2/Library.java
@@ -1,24 +1,30 @@
-
+//adding error exception
 package practica2;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Library {
 
     public static void main(String[] args) {
-    int optionInput;
+    int optionInput = 0;
     Scanner input = new Scanner (System.in);
         System.out.println("Please, select an option:");
         System.out.println("****************************");
-        System.out.println("1.Submit a book or magazine");
-        System.out.println("2.Reserve a book");
-        System.out.println("3.Reserve a magazine");
+        System.out.println("1.Submit a book");
+        System.out.println("2.Submit a magazine");
+        System.out.println("3.Reserve a book or magazine");
         System.out.println("****************************");
     
-    optionInput = input.nextInt();
+    try {
+        optionInput = input.nextInt();
+    }    
+    catch (InputMismatchException e){
+        System.out.println ("Please, select a valid option");
+    }
     
     switch (optionInput){
         case 1:
+            
             System.out.println("One");
             break;
         case 2:
